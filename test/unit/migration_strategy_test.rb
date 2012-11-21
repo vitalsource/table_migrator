5dd50063ee3d95cb59d4e0a56289ad2332cf6a94
Phi.Sanders <phi.sanders@sciphi.me>
2012-11-20 20:09:01 -0500

    test now working similar to original intent

--- test/unit/migration_strategy_test.rb
+++ test/unit/migration_strategy_test.rb
@@ -3,29 +3,46 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../test_helper.rb'))
 require 'active_record/connection_adapters/abstract/schema_definitions'
 require 'table_migrator'
 
-class MigrationStrategyTest < Test::Unit::TestCase
- # MigrationStrategy = ::TableMigrator::MigrationStrategy
-
-MigrationStrategy = ::TableMigrator::ChangeTableStrategy
-
+class ChangeTableStrategyTest < Test::Unit::TestCase
+  ChangeTableStrategy = ::TableMigrator::ChangeTableStrategy
   Table = ::ActiveRecord::ConnectionAdapters::Table
   Connection = ActiveRecord::Base.connection
 
-  context "An instance of MigrationStrategy" do
+  context "An instance of ChangeTableStrategy" do
     setup do
-      create_users
-      @strategy = MigrationStrategy.new(:users)
+      create_users unless Connection.schema_cache.table_exists?(:users)
+      @strategy = ChangeTableStrategy.new( :users, {}, ActiveRecord::Base.connection ) {}
     end
 
-    should "implement all Table methods" do
-      Table.instance_methods.each do |table_method|
-        assert @strategy.respond_to?(table_method), "Does not respond to method '#{table_method}'"
+    should "return a strategy object" do
+      assert_equal TableMigrator::ChangeTableStrategy, @strategy.class
+    end
+
+    should "represent 'Users' model" do
+      assert_equal :users, @strategy.table
+    end
+
+    should "report 'users_old' for old table name" do
+      assert_equal 'users_old', @strategy.old_table
+    end
+
+    should "report 'new_users' for new table name" do
+      assert_equal 'new_users', @strategy.new_table
+    end
+
+    should "responds for all Table methods" do
+      TableMigration.migrates(:users).change_table do |t|
+        Table.instance_methods.each do |table_method|
+          assert t.respond_to?(table_method), "Does not respond to table method '#{table_method}'"
+        end
       end
     end
 
     should "have correct existing columns list" do
-      expected_columns = Connection.columns(:users).map {|c| c.name }
-      assert_equal expected_columns.sort, @strategy.existing_columns
+      expected_columns = Connection.columns(:users).map {|c| c.name }.sort
+      assert_equal expected_columns, @strategy.column_names.sort
     end
   end
 end
+
+
