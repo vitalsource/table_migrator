0fe12850508c3c1156ffc25c3dc3a495b93ca66e
Jonathan Worek <jworek@gmail.com>
2015-10-07 17:29:36 -0400

    Adds require/include statements to allow tests to run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'shoulda'
+require 'shoulda/context'
 require 'active_record'
+require 'test/unit'
 
 TEST_ROOT = File.expand_path(File.dirname(__FILE__))
 PLUGIN_ROOT = File.expand_path(TEST_ROOT, '..')
@@ -9,4 +11,4 @@ $: << File.join(TEST_ROOT, 'lib')
 
 load 'database.rb'
 
-
+include Shoulda::Context::ClassMethods
